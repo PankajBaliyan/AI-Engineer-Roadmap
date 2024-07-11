b2474cf57079d0774aaf29e027c0a9ef79800a64
Pankaj Kumar <pankajbaliyan90@gmail.com>
2024-07-11 07:58:30 +0530

    add temp file

---
+++ Step-1-Python/temp-file.js
@@ -0,0 +1,17 @@
+let data = [
+  {
+    id: 1,
+    name: "John",
+    age: 30,
+  },
+  {
+    id: 2,
+    name: "Doe",
+    age: 25,
+  },
+  {
+    id: 3,
+    name: "Smith",
+    age: 20,
+  },
+];
